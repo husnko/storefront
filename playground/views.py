0e8a3c584e059c058e1f25d08d3bf380a7bc87df
husnko <20120637@stu.omu.edu.tr>
2022-12-12 22:26:00 +0300

    delited rubish

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 def say_hello(request):
-    name = "Husnoro"
-    x = calculate()
     return render(request, 'hello.html', {'name': 'Husnoro'})
